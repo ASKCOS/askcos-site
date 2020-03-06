17d3e670175cde345d988c6073c1acf08d70ae3e
Max Liu <mjliu@mit.edu>
2020-03-06 12:34:41 -0500

    Replace Pricer instantiation with import from globals

--- askcos_site/api/price.py
+++ askcos_site/api/price.py
@@ -1,18 +1,15 @@
 from django.http import JsonResponse
-import makeit.utilities.buyable.pricer as pricer
 
-Pricer = pricer.Pricer()
-Pricer.load()
+from askcos_site.globals import pricer
 
 def price(request):
     resp = {}
     resp['request'] = dict(**request.GET)
     smiles = request.GET.get('smiles', None)
     try:
-        p = Pricer.lookup_smiles(smiles, alreadyCanonical=True)
+        p = pricer.lookup_smiles(smiles, alreadyCanonical=True)
     except Exception as e:
         resp['error'] = str(e)
-        res.revoke()
         return JsonResponse(resp, status=400)
     resp['price'] = p
     return JsonResponse(resp)
