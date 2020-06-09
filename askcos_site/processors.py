5d41d90ad6420a4392e6bf7f2df262be672d8476
Mike Fortunato <mef231@gmail.com>
2020-06-09 16:55:12 +0000

    uses SUPPORT_EMAILS environment variable for support form

--- askcos_site/processors.py
+++ askcos_site/processors.py
@@ -5,6 +5,7 @@ def customization(request):
     customizable = {
         'organization': os.environ.get('ORGANIZATION', 'MIT'),
         'contact_email': os.environ.get('CONTACT_EMAIL', 'ccoley@mit.edu'),
+        'support_emails': os.environ.get('SUPPORT_EMAILS', 'mlpds_support@mit.edu'),
         'email_enabled': bool(os.environ.get('EMAIL_ENABLED', False)),
         'version_number': os.environ.get('VERSION_NUMBER', '0.x'),
         'update_date': os.environ.get('UPDATE_DATE', ''),
