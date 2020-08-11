be5e77f205b1c48f3a040c7e1bb572c2ec84336c
Nathaniel Kim <nkim8@mit.edu>
2020-08-10 17:10:47 -0700

    Add new ketcher iframe site; show in IPP

--- askcos_site/main/views/ketcher.py
+++ askcos_site/main/views/ketcher.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def ketcher_iframe(request):
     return render(request, 'ketcher_iframe.html')
+
+def ketcher_iframe_min(request):
+    return render(request, 'ketcher_iframe_min.html')
