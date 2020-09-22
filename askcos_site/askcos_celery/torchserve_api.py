18f6552cdffcb4643058be5b3db1436da70061bb
Max Liu <mjliu@mit.edu>
2020-09-22 19:59:02 -0400

    Refactor path ranking by shifting preprocessing to torchserve
    Also does some renaming in TorchserveAPI to simplify inheritance

--- askcos_site/askcos_celery/torchserve_api.py
+++ askcos_site/askcos_celery/torchserve_api.py
@@ -3,21 +3,21 @@ import requests
 
 class TorchserveAPI:
     """
-    Base class for torchserve API queries.
+    Base class for Torchserve API queries.
     """
 
     def __init__(self, hostname, model_name, *args, version=None, **kwargs):
         super().__init__(*args, **kwargs)
-        self.inference_url = 'http://{0}:8080/predictions/{1}'.format(hostname, model_name)
+        self.prediction_url = 'http://{0}:8080/predictions/{1}'.format(hostname, model_name)
         if version is not None:
-            self.inference_url += '/{0}'.format(version)
+            self.prediction_url += '/{0}'.format(version)
         self.management_url = 'http://{0}:8081'.format(hostname)
 
-    def inference(self, data):
+    def predict(self, data):
         """
-        Perform inference query.
+        Perform model prediction by sending request to Torchserve API.
         """
-        resp = requests.post(self.inference_url, json=data)
+        resp = requests.post(self.prediction_url, json=data)
         if resp.status_code == 200:
             return resp.json()
         else:
--- askcos_site/askcos_celery/treebuilder/path_ranking_worker.py
+++ askcos_site/askcos_celery/treebuilder/path_ranking_worker.py
@@ -2,12 +2,10 @@
 Celery worker for performing pathway ranking and clustering.
 """
 
-import scipy.sparse
 from celery import shared_task
 from celery.signals import celeryd_init
 
 from askcos.retrosynthetic.pathway_ranker import PathwayRanker
-from askcos.retrosynthetic.pathway_ranker.utils import convert_askcos_trees, tree_to_input, merge_into_batch
 from ..torchserve_api import TorchserveAPI
 
 CORRESPONDING_QUEUE = 'path_ranking_worker'
@@ -16,49 +14,10 @@ CORRESPONDING_QUEUE = 'path_ranking_worker'
 class TSPathwayRanker(TorchserveAPI, PathwayRanker):
     """
     Version of pathway ranker using torchserve to perform inference.
+
+    Uses ``TorchserveAPI.predict`` to override ``PathwayRanker.predict``.
     """
-
-    def preprocess(self, trees):
-        """
-        Convert trees into fingerprints for model input. Fingerprint arrays
-        are converted to sparse arrays in Compressed Sparse Column format
-        to reduce request size when querying the torchserve API.
-
-        One-step trees are removed from the input because they cannot be ranked.
-
-        The original indices of the remaining trees are also returned to help
-        map the results to the original list of trees.
-
-        Args:
-            trees (list): list of askcos trees to process
-
-        Returns:
-            original_indices (list): list of the original indices of the selected trees
-            batch (dict): dictionary of generated fingerprints and metadata
-        """
-        def compress(mat):
-            csc_mat = scipy.sparse.csc_matrix(mat)
-            return csc_mat.data.tolist(), csc_mat.indices.tolist(), csc_mat.indptr.tolist()
-
-        output = convert_askcos_trees(trees)
-
-        # Separate out one-step trees because they can't be ranked
-        original_indices, remaining_trees = zip(*((i, tree) for i, tree in enumerate(output) if tree['depth'] > 1))
-
-        # Convert trees to set of input fingerprints and metadata
-        batch = merge_into_batch([tree_to_input(tree) for tree in remaining_trees])
-
-        # Convert fingerprint arrays into tensors
-        batch['pfp'] = compress(batch['pfp'])
-        batch['rxnfp'] = compress(batch['rxnfp'])
-        batch['node_order'] = batch['node_order'].tolist()
-        batch['adjacency_list'] = batch['adjacency_list'].tolist()
-        batch['edge_order'] = batch['edge_order'].tolist()
-
-        return list(original_indices), batch
-
-    def postprocess(self, data):
-        return data
+    pass
 
 
 @celeryd_init.connect
