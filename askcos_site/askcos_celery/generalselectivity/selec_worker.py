fe5940cb9a0e7839c2cb31d08c4c4813af815b6e
Yanfei Guan <yanfeig@mit.edu>
2020-10-02 17:38:49 -0400

    update selectivity celery worker

--- askcos_site/askcos_celery/generalselectivity/selec_worker.py
+++ askcos_site/askcos_celery/generalselectivity/selec_worker.py
@@ -6,10 +6,13 @@ from celery import shared_task
 from celery.signals import celeryd_init
 from rdkit import RDLogger
 
+from ..atom_mapper.atom_mapping_worker import get_atom_mapping
+from ..descriptors.descriptors_worker import get_descriptors
+from askcos.synthetic.selectivity.general_selectivity import GeneralSelectivityPredictor
+
 lg = RDLogger.logger()
 lg.setLevel(RDLogger.CRITICAL)
 
-selec_pred = None
 CORRESPONDING_QUEUE = 'selec_worker'
 
 
@@ -21,24 +24,21 @@ def configure_worker(options={}, **kwargs):
     if CORRESPONDING_QUEUE not in options['queues'].split(','):
         return
     print('### STARTING UP A GENERAL SELECTIVITY PREDICTOR WORKER ###')
-    global selec_pred
-    # Import as needed
-    from askcos.synthetic.selectivity.general_selectivity import GeneralSelectivityPredictor
-    try:
-        selec_pred = GeneralSelectivityPredictor()
-    except Exception as e:
-        raise (e)
     print('Initialized')
-    configure_reac = '[Br:1][Br:2].[NH2:3][c:4]1[n:5][cH:6][n:7][c:8]2[nH:9][cH:10][n:11][c:12]12>O>' \
-                     '[Br:2][c:10]1[nH:9][c:8]2[n:7][cH:6][n:5][c:4]([NH2:3])[c:12]2[n:11]1.' \
-                     '[Br:2][c:6]1[n:5][c:4]([NH2:3])[c:12]2[c:8]([n:7]1)[nH:9][cH:10][n:11]2'
-    print(selec_pred.predict(configure_reac))
-    print('Finished configuring sites worker')
 
 
 @shared_task
-def get_selec(reac):
-    global selec_pred
+def get_selec(reac,  mapped=False, mode='qm-gnn', outcomes_included=False):
     print('site selectivity got a request {}'.format(reac))
-    res = selec_pred.predict(reac)
+
+    def mapper(rxnsmiles):
+        result = get_atom_mapping.delay(rxnsmiles)
+        return result.get(10)
+
+    def descriptors(smis):
+        result = get_descriptors.delay(smis)
+        return result.get(5)
+
+    selec_pred = GeneralSelectivityPredictor(atom_mapper=mapper, descriptor_predictor=descriptors)
+    res = selec_pred.predict(reac, mapped=mapped, mode=mode, outcomes_included=outcomes_included)
     return res
